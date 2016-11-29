5ce9fe3c3a37027f2c3f6e7e51b984f6cbc11cdf
Mike Meadows <mmeadow33@gmail.com>
2016-11-29 12:48:50 -0600

    can send tweets via app

--- index.js
+++ index.js
@@ -20,6 +20,25 @@ app.get(url.parse(config.oauth_callback).path, function(req, res) {
         }
     });
 });
+
+app.get("/tweet", (req, res) => {
+    if (!req.cookies.access_token || !req.cookies.access_token_secret) {
+        return sendStatus(401);
+    }
+    authenticator.post(`https://api.twitter.com/1.1/statuses/update.json`,
+        req.cookies.access_token, req.cookies.access_token_secret, {
+            status: "This is a test tweet to interact with twitters REST API"
+        },
+        (error, data) => {
+            if (error) {
+                return res.status(400).send(error)
+            }
+            res.send("tweet sucessful")
+        })
+})
+
+
+
 app.listen(3000, () => {
     console.log(`server is running on ${config.port}`)
 })
